fca3041d0865d9a1a73f5b25b75dde13526b66e7
roseou <598959770@qq.com>
2016-08-22 21:55:17 +0800

    tetris

--- tetris.py
+++ tetris.py
@@ -142,7 +142,7 @@ def eliminate(board):
 	return board,n
 
 def printcores(n,score,scores):
-	text = font.render("Scroes:" + str(scores), True, FONT_COLOR)
+	text = font.render("Scores:" + str(scores), True, FONT_COLOR)
 	screen.blit(text, ((COLNUM) * LEN, 0))
 
 pygame.init() 
@@ -164,8 +164,8 @@ font = pygame.font.SysFont("arial", 28)
 losefont = pygame.font.SysFont("arial", 28)
 score = 0
 scores = 0
-text = font.render("Scroes:" + str(scores), True, FONT_COLOR)
-losetext = losefont.render("Game Over!", True, FONT_COLOR)
+text = font.render("Scores:" + str(scores), True, FONT_COLOR)
+#losetext = losefont.render("Game Over!", True, FONT_COLOR)
 screen.blit(text, ((COLNUM) * LEN, 0))
 
 
